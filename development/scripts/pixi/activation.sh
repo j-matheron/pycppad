88e0acab50234abfdd4dc5824180179eabb471c7
Jeanne Matheron <jmathero@PTB-02010071.paris.inria.fr>
2025-11-18 18:33:34 +0100

    Add a pixi feature for boost.python binding - pixi feature is used by default - move binding headers to /python/include

--- development/scripts/pixi/activation.sh
+++ development/scripts/pixi/activation.sh
@@ -9,3 +9,6 @@ export CMAKE_EXPORT_COMPILE_COMMANDS=1
 
 # Activate color output with Ninja
 export CMAKE_COLOR_DIAGNOSTICS=1
+
+# Use boost.python as default binding
+export WITH_BOOST_PY_BINDING=${WITH_BOOST_PY_BINDING:=ON}
